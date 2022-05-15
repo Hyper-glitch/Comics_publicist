4ec15f937848059c8552a7831dd3ccae89c66dee
Roman <graff049@gmail.com>
2022-05-15 13:20:23 +0300

    rename vars add comment for image

--- main.py
+++ main.py
@@ -9,25 +9,27 @@ def main():
     comics_number = '353'
 
     Path(comics_path).mkdir(parents=True, exist_ok=True)
-    comics_url = get_comics_url(comics_number=comics_number)
-    save_comics_content(url=comics_url, path=comics_path)
+    comics_img = get_comics_img(comics_number=comics_number)
+    save_comics_content(url=comics_img, path=comics_path)
 
 
 def save_comics_content(url, path):
-    image_content = requests.get(url=url)
-    image_name = urllib.urlparse(url).path.split('/')[-1]
-    save_path = Path(path, image_name)
+    comics = requests.get(url=url)
+    comics_name = urllib.urlparse(url).path.split('/')[-1]
+    save_path = Path(path, comics_name)
     with open(save_path, 'wb') as image:
-        image.write(image_content.content)
+        image.write(comics.content)
 
 
-def get_comics_url(comics_number):
+def get_comics_img(comics_number):
     base_url = 'https://xkcd.com/'
     metadata = '/info.0.json'
     url = f'{base_url}{comics_number}{metadata}'
     response = requests.get(url=url)
     response.raise_for_status()
-    return response.json()['img']
+    comment = response.json()['alt']
+    image = response.json()['img']
+    return image
 
 
 if __name__ == '__main__':
