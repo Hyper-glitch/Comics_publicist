e979250d66ab7fc1cb97be524d76ccdd5413a0b9
Roman <graff049@gmail.com>
2022-05-15 14:52:26 +0300

    add get upload_img_url

--- vk_api.py
+++ vk_api.py
@@ -2,6 +2,8 @@ import urllib.parse as urllib
 
 import requests
 
+from exceptions import VkUserAuthFailed
+
 
 class VkApi:
     def __init__(self, access_token, api_version):
@@ -15,13 +17,26 @@ class VkApi:
     def get_group_id(self, group_name):
         endpoint = 'groups.get'
         url = urllib.urljoin(self.base_url, endpoint)
-        self.base_params.update({'extended': 1})
-        response = self.session.get(url=url, params=self.base_params)
+        params = dict(self.base_params.items() | {'extended': 1}.items())
+        response = self.session.get(url=url, params=params)
+
+        if response.json().get('error'):
+            message = 'User authorization failed: no access_token passed.'
+            raise VkUserAuthFailed(message=message)
 
         groups = response.json().get('response')['items']
         for group in groups:
             if group_name in group['name']:
                 return group['id']
 
-    def get_upload_img_url(self):
-        pass
+    def get_upload_img_url(self, group_id):
+        endpoint = 'photos.getWallUploadServer'
+        url = urllib.urljoin(self.base_url, endpoint)
+        params = dict(self.base_params.items() | {'group_id': group_id}.items())
+        response = self.session.get(url=url, params=params)
+
+        if response.json().get('error'):
+            message = 'User authorization failed: no access_token passed.'
+            raise VkUserAuthFailed(message=message)
+
+        return response.json()['response'].get('upload_url')
