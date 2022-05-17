c21420bb845aa8f6810cfef41de522e5f0aab1da
Roman <graff049@gmail.com>
2022-05-17 17:20:33 +0300

    1. make refactoring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import random
-import time
 import urllib.parse as urllib
 from pathlib import Path
 
@@ -10,32 +9,34 @@ from apis import VkApi, ComicsApi
 
 
 def publish_comics_vk_public():
-    """The main logic for running the whole program."""
+    """The main logic of getting random comic from 'https://xkcd.com/' and post it on the public wall in VK."""
     load_dotenv()
 
     vk_access_token = os.environ.get("VK_ACCESS_TOKEN")
     vk_api_version = 5.131
     vk_group_name = 'XKCD'
+    start_random_range = 1
 
     comics_dir = 'Files/'
     Path(comics_dir).mkdir(parents=True, exist_ok=True)
 
     comics_api = ComicsApi()
+    vk_instance = VkApi(access_token=vk_access_token, api_version=vk_api_version)
+
     last_comic = comics_api.get_comic()
-    random_comic_number = random.randint(1, last_comic['num'])
+    random_comic_number = random.randint(start_random_range, last_comic['num'])
     random_comic = comics_api.get_comic(comic_number=random_comic_number)
     random_comic_name = urllib.urlparse(random_comic['img']).path.split('/')[-1]
     comic_path = Path(comics_dir, random_comic_name)
     comics_api.save_comics_content(url=random_comic['img'], path=comic_path)
 
-    vk_instance = VkApi(access_token=vk_access_token, api_version=vk_api_version)
     group_id = vk_instance.get_group_id(group_name=vk_group_name)
-    upload_img_url = vk_instance.get_upload_img_url(group_id=group_id)
-    upload_img_info = vk_instance.upload_img_to_server(img_path=comic_path, upload_url=upload_img_url)
-    saved_img_info = vk_instance.save_img_to_public(upload_img_info=upload_img_info)
+    uploaded_img_url = vk_instance.get_upload_img_url(group_id=group_id)
+    uploaded_img = vk_instance.upload_img_to_server(img_path=comic_path, upload_url=uploaded_img_url)
+    saved_img = vk_instance.save_img_to_public(upload_img_info=uploaded_img)
     vk_instance.post_img(
-        message=last_comic['alt'], media_id=saved_img_info['id'],
-        owner_id=saved_img_info['owner_id'], group_id=group_id,
+        message=random_comic['alt'], media_id=saved_img['id'],
+        owner_id=saved_img['owner_id'], group_id=group_id,
     )
     if os.path.isfile(comic_path) or os.path.islink(comic_path):
         os.remove(comic_path)
