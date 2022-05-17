5ff3875cefecd64fba93810ca5907f6612adf541
Roman <graff049@gmail.com>
2022-05-17 23:35:31 +0300

    1. add docstrings for VkApi

--- apis.py
+++ apis.py
@@ -18,14 +18,22 @@ class VkApi:
         self.session = requests.Session()
 
     def get_json(self, url: str, params: dict) -> dict:
-        """Get content in json format."""
+        """Get content in json format, raise if error response.
+        :param url: - url for make a get request.
+        :param params: - needed parameters for make a success request.
+        :returns: comic_info - all received information about comic.
+        """
         response = self.session.get(url=url, params=params)
         jsonify_response = response.json()
         error = jsonify_response.get('error')
         self.check_on_error(error, exception=VkUserAuthFailed)
         return jsonify_response['response']
 
-    def get_group_id(self, group_name):
+    def get_group_id(self, group_name: str) -> int:
+        """Get content in json format, raise if error response.
+        :param group_name: - group name for getting its id.
+        :returns: group_id - group id.
+        """
         endpoint = 'groups.get'
         url = urllib.urljoin(self.base_url, endpoint)
         params = {'extended': 1}
@@ -36,7 +44,11 @@ class VkApi:
             if group_name in group['name']:
                 return group['id']
 
-    def get_upload_img_url(self, group_id):
+    def get_upload_img_url(self, group_id: int) -> str:
+        """Get url for upload an image to the server.
+        :param group_id: - group name for getting its id.
+        :returns: upload_img_url - url for upload an image.
+        """
         endpoint = 'photos.getWallUploadServer'
         url = urllib.urljoin(self.base_url, endpoint)
         params = {'group_id': group_id}
@@ -44,7 +56,12 @@ class VkApi:
         upload_img_url = self.get_json(url=url, params=self.base_params).get('upload_url')
         return upload_img_url
 
-    def upload_img_to_server(self, img_path, upload_url):
+    def upload_img_to_server(self, img_path: str, upload_url: str) -> dict:
+        """Make post request for uploading an image to the server.
+        :param img_path: - path to an image.
+        :param upload_url: - url for upload an image.
+        :returns: upload_img_info - needed information for save the image to vk public.
+        """
         with open(f'{img_path}', 'rb') as image:
             params = {'photo': image}
             response = self.session.post(url=upload_url, files=params)
@@ -54,7 +71,11 @@ class VkApi:
         self.check_on_error(error, exception=UploadPhotoError)
         return upload_img_info
 
-    def save_img_to_public(self, upload_img_info):
+    def save_img_to_public(self, upload_img_info: dict) -> dict:
+        """Make post request for saving an image to the public vk.
+        :param upload_img_info: - needed information for save the image to vk public.
+        :returns: saved_img_info - all information of saved image.
+        """
         endpoint = 'photos.saveWallPhoto'
         url = urllib.urljoin(self.base_url, endpoint)
         upload_img_info.update(self.base_params)
@@ -66,6 +87,13 @@ class VkApi:
         return saved_img_info
 
     def post_img(self, message, media_id, owner_id, group_id):
+        """Make post request for saving an image to the public vk.
+        :param message: - The text of the message (required if the attachments parameter is not set).
+        :param media_id: - Media application identifier.
+        :param owner_id: - ID of the owner of the media application.
+        :param group_id: - The ID of the user or community on whose wall the post is to be posted.
+        :returns: post_id - Upon successful completion, returns the ID of the created record.
+        """
         endpoint = 'wall.post'
         media_type = 'photo'
         url = urllib.urljoin(self.base_url, endpoint)
@@ -85,7 +113,11 @@ class VkApi:
         return post_id
 
     @staticmethod
-    def check_on_error(error, exception):
+    def check_on_error(error: dict, exception):
+        """Check and raise an exception if response has errors.
+        :param error: - Error parameter from response.
+        :param exception: - Exception depending on error.
+        """
         if error:
             message = error['error_msg']
             raise exception(message)
@@ -101,6 +133,7 @@ class ComicsApi:
     def get_comic(self, comic_number: int = None) -> dict:
         """Get comic information in json format.
         :param comic_number: - comic index number.
+        :returns: comic_info - all received information about comic.
         """
         if comic_number:
             url = f'{self.base_url}{comic_number}{self.data_format}'
