f634629614540c01a353a666f7f57bade108c6df
Roman <graff049@gmail.com>
2022-05-17 15:37:40 +0300

    1. make refactor 2. publish comics eternally

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 import random
-import urllib.parse as urllib
+import time
 from pathlib import Path
 
-import requests
 from dotenv import load_dotenv
 
-from apis import VkApi
+from apis import VkApi, ComicsApi
 
 
-def publish_comics_to_vk():
+def publish_comics_vk_public():
     """The main logic for running the whole program."""
     load_dotenv()
 
@@ -18,12 +17,13 @@ def publish_comics_to_vk():
     vk_group_name = 'XKCD'
 
     comics_path = 'Files/'
-
     Path(comics_path).mkdir(parents=True, exist_ok=True)
-    last_comic = get_last_comic()
-    random_comic = random.randint(1, last_comic['num'])
 
-    save_comics_content(url=last_comic['img'], path=comics_path)
+    comics_api = ComicsApi()
+    last_comic = comics_api.get_comic()
+    random_comic_number = random.randint(1, last_comic['num'])
+    random_comic = comics_api.get_comic(comic_number=random_comic_number)
+    comics_api.save_comics_content(url=random_comic['img'], path=comics_path)
 
     vk_instance = VkApi(access_token=vk_access_token, api_version=vk_api_version)
     group_id = vk_instance.get_group_id(group_name=vk_group_name)
@@ -36,31 +36,7 @@ def publish_comics_to_vk():
     )
 
 
-def save_comics_content(url, path):
-    comics = requests.get(url=url)
-    comics_name = urllib.urlparse(url).path.split('/')[-1]
-    save_path = Path(path, comics_name)
-    with open(save_path, 'wb') as image:
-        image.write(comics.content)
-
-
-def get_last_comic():
-    base_url = 'https://xkcd.com/'
-    metadata = '/info.0.json'
-    url = f'{base_url}{metadata}'
-    response = requests.get(url=url)
-    response.raise_for_status()
-    return response.json()
-
-
-def get_comic(comic_number):
-    base_url = 'https://xkcd.com/'
-    metadata = '/info.0.json'
-    url = f'{base_url}{comic_number}{metadata}'
-    response = requests.get(url=url)
-    response.raise_for_status()
-    return response.json()
-
-
 if __name__ == '__main__':
-    publish_comics_to_vk()
+    while True:
+        publish_comics_vk_public()
+        time.sleep(5)
