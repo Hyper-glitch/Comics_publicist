b8dae36ed775811edde5c92e7aae799c0f7da0a6
Roman <graff049@gmail.com>
2022-05-17 23:43:43 +0300

    1. add publication_frequency = 1 time a day 2. add while true loop for it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 import urllib.parse as urllib
 from pathlib import Path
 
@@ -13,6 +14,7 @@ def publish_comics_vk_public():
     load_dotenv()
 
     vk_access_token = os.environ.get("VK_ACCESS_TOKEN")
+    publication_frequency = float(os.environ.get("PUBLICATION_FREQUENCY"))
     vk_api_version = 5.131
     vk_group_name = 'XKCD'
     start_random_range = 1
@@ -22,24 +24,25 @@ def publish_comics_vk_public():
     comics_api = ComicsApi()
     vk_instance = VkApi(access_token=vk_access_token, api_version=vk_api_version)
 
-    last_comic = comics_api.get_comic()
-    random_comic_number = random.randint(start_random_range, last_comic['num'])
-    random_comic = comics_api.get_comic(comic_number=random_comic_number)
-    random_comic_name = urllib.urlparse(random_comic['img']).path.split('/')[-1]
-    comic_path = Path(comics_dir, random_comic_name)
-    comics_api.save_comics_content(url=random_comic['img'], path=comic_path)
+    while True:
+        last_comic = comics_api.get_comic()
+        random_comic_number = random.randint(start_random_range, last_comic['num'])
+        random_comic = comics_api.get_comic(comic_number=random_comic_number)
+        random_comic_name = urllib.urlparse(random_comic['img']).path.split('/')[-1]
+        comic_path = Path(comics_dir, random_comic_name)
+        comics_api.save_comics_content(url=random_comic['img'], path=comic_path)
 
-    group_id = vk_instance.get_group_id(group_name=vk_group_name)
-    uploaded_img_url = vk_instance.get_upload_img_url(group_id=group_id)
-    uploaded_img = vk_instance.upload_img_to_server(img_path=comic_path, upload_url=uploaded_img_url)
-    saved_img = vk_instance.save_img_to_public(upload_img_info=uploaded_img)
-    vk_instance.post_img(
-        message=random_comic['alt'], media_id=saved_img['id'],
-        owner_id=saved_img['owner_id'], group_id=group_id,
-    )
-    if os.path.isfile(comic_path) or os.path.islink(comic_path):
-        os.remove(comic_path)
-    return
+        group_id = vk_instance.get_group_id(group_name=vk_group_name)
+        uploaded_img_url = vk_instance.get_upload_img_url(group_id=group_id)
+        uploaded_img = vk_instance.upload_img_to_server(img_path=comic_path, upload_url=uploaded_img_url)
+        saved_img = vk_instance.save_img_to_public(upload_img_info=uploaded_img)
+        vk_instance.post_img(
+            message=random_comic['alt'], media_id=saved_img['id'],
+            owner_id=saved_img['owner_id'], group_id=group_id,
+        )
+        if os.path.isfile(comic_path) or os.path.islink(comic_path):
+            os.remove(comic_path)
+        time.sleep(publication_frequency)
 
 
 if __name__ == '__main__':
